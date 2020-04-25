4721c644e82f0503bad9760ed00c3fa8d6e5595b
Felipe Balbontin <felipee.b95@gmail.com>
2020-04-25 18:25:27 -0400

    route for board

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: BoardComponent },
+  { path: '**', component: BoardComponent }, // Wildcard route for a 404 page
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
